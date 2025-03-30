9defe0dd12c89ef483bfded5a39387052e40ee76
rohitsinghme1 <rohitsinghme1@gmail.com>
2025-03-30 15:29:53 +0530

    dashbord

--- Placement/src/App.jsx
+++ Placement/src/App.jsx
@@ -1,19 +1,16 @@
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
-import StudentLogin from "./pages/Login/Student_Login.jsx";
+import Dashboard from "./pages/Dashboard/Dashboard.jsx";
+// import StudentLogin from "./pages/Login/Student_Login.jsx";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import UniversityLogin from "./pages/University_Login/University_Login.jsx";
+// import UniversityLogin from "./pages/University_Login/University_Login.jsx";
+
 function App() {
 
   return (
     <div>
-      <Router>
-          <Routes>
-              <Route path="/" element={<StudentLogin />} />
-              <Route path ="/University_Login" element={<UniversityLogin />} />
-          </Routes>
-      </Router>
+        <Dashboard />
     </div>
   )
 }
